474d9ad8c49c288ba84e7d3d290ef0f2d937dc4b
georgetim1998 <georgetim1998@gmail.com>
2022-06-28 01:53:39 +0300

    test and added associations

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
+  describe 'associations' do
+    it { should belong_to(:question) } 
+  end
+
   context 'validations' do
     it { should validate_presence_of(:body) }
   end
