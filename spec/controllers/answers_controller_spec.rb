1c1ad60cc332820a0a3c1a140078f1f0b1ec0c7d
georgetim1998 <georgetim1998@gmail.com>
2022-07-01 14:10:24 +0300

    refactoring

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -27,19 +27,20 @@ RSpec.describe AnswersController, type: :controller do
         expect { post :create, params: { question_id: question.id, answer: attributes_for(:answer) } }.to change(question.answers, :count).by(1)
       end
       
-      it "redirects to show view" do
+      it 'redirects to show view' do
         post :create, params: { question_id: question.id, answer: attributes_for(:answer) }
         expect(response).to redirect_to assigns(:answer) 
       end
     end
     
     context 'with invalid attributes' do
+      before { post :create, params: { question_id: question.id, answer: attributes_for(:answer, :invalid) } }
+
       it 'doesnt save a new answer with invalid parameters' do
-        expect { post :create, params: { question_id: question.id, answer: attributes_for(:answer, :invalid) } }.to_not change(question.answers, :count)
+        expect { response }.to_not change(question.answers, :count)
       end
       
       it "renders new template" do
-        post :create, params: { question_id: question.id, answer: attributes_for(:answer, :invalid) }
         expect(response).to render_template :new 
       end
     end
