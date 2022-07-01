63ecc8a1ee85a2fe28833d0a9a0e58085734639e
georgetim1998 <georgetim1998@gmail.com>
2022-07-01 12:36:13 +0300

    new answer create

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,17 @@
 class AnswersController < ApplicationController
+  before_action :find_question, only: %i[new]
+
+  def new
+    @answer = @question.answers.build
+  end
+
+  private 
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
+
+  def find_question
+    @question = Question.find(params[:question_id])
+  end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
+  subject(:question) { create(:question) }
 
+  describe 'GET #new' do
+    before { get :new, params: { question_id: question.id } }
+
+    it 'assigns a new Answer to @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'assigns @question to @answer' do
+      expect(assigns(:answer).question).to eq question
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
 end
