9f8f69052d3799183b4d80a1b145a41984500a1d
georgetim1998 <georgetim1998@gmail.com>
2022-07-13 19:56:11 +0300

    sign out tests

---
+++ spec/features/user/sign_up_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'User signs up' do
+  background { visit new_user_registration_path }
+
+  scenario 'with valid params' do
+    fill_in 'Email', with: 'my@string.com'
+    fill_in 'Password', with: 'MyString'
+    fill_in 'Password confirmation', with: 'MyString'
+
+    click_on 'Sign up'
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'    
+  end
+
+  scenario 'with invalid params' do
+    click_on 'Sign up'
+    expect(page).to have_content 'errors prohibited this user from being saved:'
+  end
+end
