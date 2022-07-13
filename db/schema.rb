ddac64b4703d1c9f847ea7595601ad44f61a3ce3
georgetim1998 <georgetim1998@gmail.com>
2022-07-13 18:38:54 +0300

    add devise to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_627_221_253) do
+ActiveRecord::Schema.define(version: 20_220_713_152_741) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20_220_627_221_253) do
     t.text 'body', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+  end
+
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
   end
 
   add_foreign_key 'answers', 'questions'
