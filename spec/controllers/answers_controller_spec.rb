939ed98ca5d9edbbb2c2c8d35b66f2fa4a4baff4
georgetim1998 <georgetim1998@gmail.com>
2022-07-15 17:39:57 +0300

    tries to answer

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe AnswersController, type: :controller do
 
       it 'redirects to show view' do
         post :create, params: { question_id: question.id, answer: attributes_for(:answer) }
-        expect(response).to redirect_to assigns(:answer)
+        expect(response).to redirect_to question_path(question)
       end
     end
 
---
+++ spec/features/answer/create_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature 'Create answer' do
+  given(:question) { create(:question) }
+
+  describe 'Authenticated user'  do
+    given(:user) { create(:user) }
+
+    background { sign_in(user) }
+    background { visit question_path(question) }
+
+    scenario 'tries to answer' do
+      fill_in 'Body', with: 'Some text'
+      click_on 'Reply'
+      
+      expect(page).to have_content 'Your answer has been sent successfully.'
+      expect(page).to have_content 'Some text'
+    end
+
+    scenario 'tries to send an answer with errors' 
+  end
+
+  describe 'Unauthenticated user' do
+    scenario 'tries to send an answer'
+  end
+end
