f839e0b7b9fedce213a83e3b89c1dff966c8da3f
georgetim1998 <georgetim1998@gmail.com>
2022-07-15 16:23:53 +0300

    View a questions with answers

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -6,5 +6,18 @@ FactoryBot.define do
     trait :invalid do
       title { nil }
     end
+    
+    factory :question_with_answers do
+      title { 'MyString1' }
+      body { 'MyText1' }
+      
+      transient do
+        answers_count { 2 }
+      end
+      
+      answers do
+        Array.new(answers_count) { association(:answer) }
+      end
+    end
   end
 end
