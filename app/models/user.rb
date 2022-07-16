2a12363af9edd69e3c6f69c86ab09090d686bbfd
georgetim1998 <georgetim1998@gmail.com>
2022-07-16 19:08:43 +0300

    author association for answer with test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :validatable
 
   has_many :questions, inverse_of: 'author', foreign_key: 'author_id', dependent: :destroy
+  has_many :answers, inverse_of: 'author', foreign_key: 'author_id', dependent: :destroy
 
   def author_of?(resource)
     resource.author_id == id
