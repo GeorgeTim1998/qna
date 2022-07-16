dee1378d68e2054ca298c59da61822aed4adcaec
georgetim1998 <georgetim1998@gmail.com>
2022-07-16 18:08:29 +0300

    Tests for association

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe Question, type: :model do
     it { should have_many(:answers).dependent(:destroy) }
   end
 
+  context 'with author' do
+    it { is_expected.to belong_to(:author).class_name('User') }
+  end
+
   context 'validations' do
     it { should validate_presence_of(:body) }
     it { should validate_presence_of(:title) }
