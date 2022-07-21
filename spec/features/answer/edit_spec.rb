488fe3802e9c4781bfde59767dd3e1b865210a5e
georgetim1998 <georgetim1998@gmail.com>
2022-07-21 20:56:03 +0300

    edit answer update

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -5,20 +5,21 @@ feature 'User can edit his answer' do
   given!(:user) { create(:user) }
   given!(:question) { create(:question) }
   given!(:answer) { create(:answer, question: question, author: user) }
+  given!(:another_user) { create(:user) }
 
   scenario 'Unauthenticated can not edit answer' do
     visit question_path(question)
 
     expect(page).to_not have_link 'Edit'
   end
-
+  
   describe 'Authenticated user', js: true do
     scenario 'edits his answer' do
       sign_in user
       visit question_path(question)
-
+      
       click_on 'Edit answer'
-
+      
       within '.answers' do
         fill_in 'Your answer', with: 'edited answer'
         click_on 'Save'
@@ -29,7 +30,24 @@ feature 'User can edit his answer' do
       end
     end
 
-    scenario 'edits his answer with errors'
-    scenario "tries to edit other user's question"
+    scenario 'edits his answer with errors' do
+      sign_in user
+      visit question_path(question)
+      click_on 'Edit answer'
+      
+      within '.answers' do
+        fill_in 'Your answer', with: ''
+        click_on 'Save'
+        
+        expect(page).to have_content answer.body
+      end
+    end
+    
+    scenario "tries to edit other user's question" do
+      sign_in(another_user)
+      visit question_path(question)
+      
+      expect(page).to_not have_link 'Edit answer'
+    end
   end
 end
