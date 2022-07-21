dbf3781ff15ee5b6176a4ac26dc030f645b93713
georgetim1998 <georgetim1998@gmail.com>
2022-07-21 16:41:32 +0300

    answer controller spec uodate create action

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -28,25 +28,16 @@ RSpec.describe AnswersController, type: :controller do
       it 'saves a new answer with a foreign key to the question to the database' do
         expect do
           post :create,
-               params: { question_id: question.id, answer: attributes_for(:answer) }
+               params: { question_id: question.id, answer: attributes_for(:answer), format: :js }
         end.to change(question.answers, :count).by(1)
-      end
-
-      it 'redirects to show view' do
-        post :create, params: { question_id: question.id, answer: attributes_for(:answer) }
-        expect(response).to redirect_to question_path(question)
       end
     end
 
     context 'with invalid attributes' do
-      before { post :create, params: { question_id: question.id, answer: attributes_for(:answer, :invalid) } }
+      before { post :create, params: { question_id: question.id, answer: attributes_for(:answer, :invalid) }, format: :js }
 
       it 'doesnt save a new answer with invalid parameters' do
         expect { response }.to_not change(question.answers, :count)
-      end
-
-      it 'renders questions/show template' do
-        expect(response).to render_template 'questions/show'
       end
     end
   end
