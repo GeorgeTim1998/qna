3148be02e35161d858020d565af9ff207537d7e5
georgetim1998 <georgetim1998@gmail.com>
2022-07-22 16:15:40 +0300

    addition^ answer model spec

--- db/schema.rb
+++ db/schema.rb
@@ -10,38 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_716_160_507) do
+ActiveRecord::Schema.define(version: 2022_07_22_124451) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'answers', force: :cascade do |t|
-    t.text 'body', null: false
-    t.bigint 'question_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'author_id', null: false
-    t.index ['author_id'], name: 'index_answers_on_author_id'
-    t.index ['question_id'], name: 'index_answers_on_question_id'
+  create_table "answers", force: :cascade do |t|
+    t.text "body", null: false
+    t.bigint "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "author_id", null: false
+    t.boolean "best", default: false, null: false
+    t.index ["author_id"], name: "index_answers_on_author_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
-  create_table 'questions', force: :cascade do |t|
-    t.string 'title', null: false
-    t.text 'body', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'author_id', null: false
-    t.index ['author_id'], name: 'index_questions_on_author_id'
+  create_table "questions", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_questions_on_author_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key 'answers', 'questions'
-  add_foreign_key 'answers', 'users', column: 'author_id'
-  add_foreign_key 'questions', 'users', column: 'author_id'
+  add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users", column: "author_id"
+  add_foreign_key "questions", "users", column: "author_id"
 end
