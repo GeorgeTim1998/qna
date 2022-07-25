e9539299846e9ab1e327be03138fdae184dc6f6d
georgetim1998 <georgetim1998@gmail.com>
2022-07-25 19:11:51 +0300

    user edits file

--- spec/features/question/edit_file_spec.rb
+++ spec/features/question/edit_file_spec.rb
@@ -24,5 +24,18 @@ feature 'Editing the files' do
     end
   end
 
-  scenario 'Unauthorized user tries to edit attached files'
+  scenario 'Unauthorized user tries to edit attached files' do
+    sign_in(another_user)
+    visit question_path(question)
+
+    expect(page).to have_link 'README.md'
+    expect(page).to have_no_content 'Edit the question'
+  end
+
+  scenario 'Unauthenticated user tries to edit attached files' do
+    visit question_path(question)
+
+    expect(page).to have_link 'README.md'
+    expect(page).to have_no_content 'Edit the question'
+  end
 end
