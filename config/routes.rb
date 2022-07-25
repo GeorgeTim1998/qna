bc949f80eca9807c301c3ec349f3ffc5f49648b0
georgetim1998 <georgetim1998@gmail.com>
2022-07-26 01:04:05 +0300

    attachment controller spec

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'questions#index'
 
+  resources :attachments, only: :destroy
+
   resources :questions, only: %i[show new create destroy update] do
     resources :answers, only: %i[show new create destroy update], shallow: true do
       member do
