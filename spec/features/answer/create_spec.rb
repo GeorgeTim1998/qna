5b1f74014053c6c02608703b5bb6d3808702ff2e
georgetim1998 <georgetim1998@gmail.com>
2022-07-25 09:43:26 +0300

    user can add multiple files when asking a question

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -23,6 +23,17 @@ feature 'Create answer', js: true do
 
       expect(page).to have_content "Body can't be blank"
     end
+
+    scenario 'asks a answer with attached file' do
+      fill_in 'Body', with: 'Some text'
+      click_on 'Reply'
+
+      attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+      click_on 'Reply'
+
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
   end
 
   describe 'Unauthenticated user' do
