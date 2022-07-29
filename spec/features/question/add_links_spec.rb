e33f47da9657c903ae0f30d70640078fe268bd3d
georgetim1998 <georgetim1998@gmail.com>
2022-07-29 17:56:19 +0300

    several links for question

--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -3,19 +3,32 @@ require 'rails_helper'
 feature do
   given(:user) { create(:user) }
   given(:gist_url) { 'https://gist.github.com/vkurennov/743f9367caa1039874af5a2244e1b44c' }
+  given(:another_url) { 'https://www.google.com/' }
 
-  scenario 'User adds link when asks question' do
+  scenario 'User adds link when asks question', js: true do
     sign_in(user)
     visit new_question_path
 
     fill_in 'Title', with: 'Test question'
     fill_in 'Body', with: 'text text text'
 
-    fill_in 'Link name', with: 'My gist'
-    fill_in 'Url', with: gist_url
-
+    within('#links') do
+      fill_in 'Link name', with: 'My gist'
+      fill_in 'Url', with: gist_url
+      
+      click_on 'Add link'
+      
+      within all('.nested-fields').last do
+        fill_in 'Link name', with: 'Google'
+        fill_in 'Url', with: another_url
+      end
+    end
+    
     click_on 'Ask'
-
-    expect(page).to have_link 'My gist', href: gist_url
+    
+    within('.links') do
+      expect(page).to have_link 'My gist', href: gist_url
+      expect(page).to have_link 'Google', href: another_url
+    end
   end
 end
