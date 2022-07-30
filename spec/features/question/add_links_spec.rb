4e225525523a004dd4dce1441ac90c26288020ae
georgetim1998 <georgetim1998@gmail.com>
2022-07-30 13:00:02 +0300

    small refactor

--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -5,32 +5,50 @@ feature do
   given(:gist_url) { 'https://gist.github.com/vkurennov/743f9367caa1039874af5a2244e1b44c' }
   given(:another_url) { 'https://www.google.com/' }
 
-  scenario 'User adds link when asks question', js: true do
-    sign_in(user)
-    visit new_question_path
-
-    fill_in 'Title', with: 'Test question'
-    fill_in 'Body', with: 'text text text'
-
-    within('#links') do
-      fill_in 'Link name', with: 'My gist'
-      fill_in 'Url', with: gist_url
-
-      click_on 'Add link'
-
-      within all('.nested-fields').last do
-        fill_in 'Link name', with: 'Google'
-        fill_in 'Url', with: another_url
+  describe "User adds" do
+    before do
+      sign_in(user)
+      visit new_question_path
+      
+      fill_in 'Title', with: 'Test question'
+      fill_in 'Body', with: 'text text text'
+    end
+    
+    scenario 'link when asks question', js: true do
+      within('#links') do
+        fill_in 'Link name', with: 'My gist'
+        fill_in 'Url', with: gist_url
+  
+        click_on 'Add link'
+  
+        within all('.nested-fields').last do
+          fill_in 'Link name', with: 'Google'
+          fill_in 'Url', with: another_url
+        end
+      end
+  
+      click_on 'Ask'
+  
+      within('.question-links') do
+        expect(page).to have_link 'My gist', href: gist_url
+        expect(page).to have_link 'Google', href: another_url
       end
     end
-
-    click_on 'Ask'
-
-    within('.question-links') do
-      expect(page).to have_link 'My gist', href: gist_url
-      expect(page).to have_link 'Google', href: another_url
+  
+    scenario 'attaches gist', js: true do
+      within('#links') do
+        fill_in 'Link name', with: 'My gist'
+        fill_in 'Url', with: gist_url
+      end
+  
+      click_on 'Ask'
+  
+      within('.question-links') do
+        expect(page).to have_link 'My gist', href: gist_url
+      end
     end
   end
+  
 
   describe 'User edits a question', js: true do
     before { sign_in(user) }
