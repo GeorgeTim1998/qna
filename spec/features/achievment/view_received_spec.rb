fe35a8c66b5e457eaf987a074ec9b9b8c01632f9
georgetim1998 <georgetim1998@gmail.com>
2022-08-01 00:29:37 +0300

    user views achievement

---
+++ spec/features/achievment/view_received_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'The user can view achievements.' do
+  let(:user) { create(:user) }
+  let!(:received_achievement) { create(:achievement, name: 'Achievement23', winner: user) }
+
+  describe 'Authenticated user' do
+    before do
+      sign_in(user)
+      visit achievements_path
+    end
+
+    scenario 'tries view received achievements' do
+      click_on 'Your achievements'
+      
+      expect(page).to have_content received_achievement.name
+      expect(page).to have_content received_achievement.question.title
+      expect(page).to have_xpath "//img[contains(@src, 'price')]"
+    end
+  end
+end
