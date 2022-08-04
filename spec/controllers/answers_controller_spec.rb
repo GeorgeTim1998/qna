e5e0e7fbb7b735b66e79eddb00c8a125b4be5810
georgetim1998 <georgetim1998@gmail.com>
2022-08-04 18:33:39 +0300

    rubo -a

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe AnswersController, type: :controller do
   let(:user) { create(:user) }
   before { sign_in(user) }
-  
+
   include_examples 'voting', :answer
-  
+
   subject(:question) { create(:question, author: user) }
 
   describe 'GET #new' do
