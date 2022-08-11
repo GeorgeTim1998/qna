840a2040526038cfad1cff8c473abc25476cc3b6
georgetim1998 <georgetim1998@gmail.com>
2022-08-11 23:19:19 +0300

    can ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,32 +1,33 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the user here. For example:
-    #
-    #   return unless user.present?
-    #   can :read, :all
-    #   return unless user.admin?
-    #   can :manage, :all
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, published: true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
+    can :read, :all
+
+    if user.present?
+      can :create, :all
+      can :received, Achievement
+
+      ability_to_vote(user)
+      abilities_for_author(user)
+    end
+  end
+
+  private
+
+  def abilities_for_author(user)
+    can %i[update destroy], [Question, Answer], author_id: user.id
+    can :destroy, ActiveStorage::Attachment, record: { author_id: user.id }
+    can :best, Answer, question: { author_id: user.id }
+  end
+
+  def ability_to_vote(user)
+    can :change_rating, [Question, Answer] do |resource|
+      !user.voted_for?(resource) && !user.author_of?(resource)
+    end
+
+    can :cancel, [Question, Answer] do |resource|
+      user.voted_for?(resource) && !user.author_of?(resource)
+    end
   end
 end
